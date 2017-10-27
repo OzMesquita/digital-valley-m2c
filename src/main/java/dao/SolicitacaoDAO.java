a32ea1a40e36056c1863a30d234f3b6d20a6d9fc
Deyvison Nogueira <deyvison_s@hotmail.com>
2017-10-27 13:55:10 -0300

    merge

--- src/main/java/dao/SolicitacaoDAO.java
+++ src/main/java/dao/SolicitacaoDAO.java
@@ -8,9 +8,13 @@ import br.com.n2s.model.Solicitacao;
 public interface SolicitacaoDAO {
 	
 	public void cadastrar(Solicitacao solicitacao);
+	
 	public void buscarPorId(int id);
+	
 	public void buscarPorAluno(Aluno aluno);
+	
 	public void buscarPorProfessor(Professor professor);
+	
 	public void buscarPorTipo(EnumSolicitacao tipo);
 		
 }
