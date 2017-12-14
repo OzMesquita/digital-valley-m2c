b0c19166ac20d8b50eaf3cb10b80d7c17b605b81
Deyvison Nogueira <deyvison_s@hotmail.com>
2017-12-14 14:40:43 -0300

    Conserto de erros

--- src/main/java/util/FacadeSolicitacoes.java
+++ src/main/java/util/FacadeSolicitacoes.java
@@ -129,26 +129,16 @@ public class FacadeSolicitacoes {
 
 					Email e = new Email();
 					e.sendEmailWithAttachment("Solicitação de Segunda chamada", msg,
-<<<<<<< HEAD
-							solicitacao.getProfessor().getEmail(), "Usuário Controle de Acesso", localArquivo);
-=======
 							solicitacao.getProfessor().getEmail(), solicitacao.getProfessor().getNome(), localArquivo, nomeArquivo);
->>>>>>> 64ffeeef9d884fb96795fd73c1246d81e028ff45
 
 				} else if (solicitacao.getTipoSolicitacao().equals(EnumSolicitacao.RECORRECAO)) {
 					msg = "O(A) Aluno(a) " + solicitacao.getAluno().getNome() + " solicitou a Recorreção da prova de "
 							+ solicitacao.getDisciplina().getNome() + " realizada na data de: "
-<<<<<<< HEAD
-							+ FacadeSolicitacoes.converterLocalDateTimeToString(solicitacao.getDataEHoraProva())
-							+ " com a justificativa de : \"" + solicitacao.getJustificativa() + "\"";
-
-=======
 							+ FacadeSolicitacoes.converterLocalDateTimeToString(solicitacao.getDataEHoraProva()) + ", "
 							+ "que teve o resultado divulgado em "
 							+ FacadeSolicitacoes
 									.converterLocalDateToString(solicitacao.getDataDivulgacaoResultadoProva())
 							+ " com a justificativa de : \"" + solicitacao.getJustificativa() + "\"";
->>>>>>> 64ffeeef9d884fb96795fd73c1246d81e028ff45
 					Email e = new Email();
 					e.sendEmailWithAttachment("Solicitação de Recorreção", msg,
 							solicitacao.getDisciplina().getCurso().getCoordenador().getEmail(),
